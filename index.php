b3bd7265c8361bf897ba4172d567402619ad85f5
ccsoler09 <75436539+ccsoler09@users.noreply.github.com>
2021-03-07 21:40:44 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+$controller = isset($_GET['controller'])? $_GET['controller']: 'index';
+$method = isset($_GET['method'])? $_GET['method']: 'index';
+
+spl_autoload_register(function ($class){
+  if (file_exists("controllers/{$class}.php")){
+    require_once "controllers/{$class}.php";
+  }elseif (file_exists("models/{$class}.php")){
+    require_once "models/{$class}.php";
+  }else{
+    die('No se logro encontrar el archivo');
+  }
+});
+
+$controller ="{$controller}Controller";
+$controller = new $controller();
+
+call_user_func([$controller, $method]);
+
+?>
